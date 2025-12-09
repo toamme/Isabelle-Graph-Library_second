848c5b4f9eec2e9ea77d39ab20518ddbc0f05db0
toamme <116581197+toamme@users.noreply.github.com>
2025-12-09 19:57:27 +0000

    iterators experiment

--- Set_Graphs/Graph_Algorithms/BFS_Subprocedures_Imperative.thy
+++ Set_Graphs/Graph_Algorithms/BFS_Subprocedures_Imperative.thy
@@ -1,4 +1,64 @@
 theory BFS_Subprocedures_Imperative
   imports BFS_Subprocedures Directed_Set_Graphs.Pair_Graph_Imperative
 begin
+
+hide_const RBT_Set.ins
+
+locale double_naturals = 
+imp_set_iterate where is_set = is_set and it_next = it_next +
+imp_set_ins where ins = ins and is_set = is_set +
+imp_set_empty where empty = empty and is_set = is_set
+for is_set :: "nat set \<Rightarrow> 'simpl \<Rightarrow> assn" 
+and ins :: "nat \<Rightarrow> 'simpl \<Rightarrow> 'simpl Heap"
+and empty :: "'simpl Heap"
+and it_next :: "'it \<Rightarrow> (nat \<times> 'it) Heap"
+ +
+fixes S::"nat set"
+and  Simpl::'simpl
+begin
+
+
+partial_function (heap) double_all_elems_loop ::"'simpl \<Rightarrow> 'it \<Rightarrow> 'simpl Heap"
+  where
+"double_all_elems_loop simpl it = 
+  (do {b <- it_has_next it;
+       if \<not> b then  return simpl
+       else  do {
+         (x, it') <- it_next it;
+          simpl' <- ins (2*x) simpl;
+          double_all_elems_loop simpl' it'
+       }})"
+lemmas [code] = double_all_elems_loop.simps
+end
+
+(*interpretation hs: imp_set_iterate 
+  is_hashset hs_is_it hs_it_init hs_it_has_next hs_it_next
+  by (rule hs_iterate_impl)*)
+
+definition "from_list_impl (xs:: nat list) \<equiv>
+  do {S <- hs_new; foldM (\<lambda> x S. hs_ins  x S) xs S }"
+
+  setup Locale_Code.open_block  
+interpretation dbl_nats_hs: double_naturals
+ hs_is_it hs_it_init hs_it_has_next is_hashset hs_ins hs_new hs_it_next
+  by unfold_locales
+setup Locale_Code.close_block
+
+definition "double_all_elems_loop = dbl_nats_hs.double_all_elems_loop"
+
+ find_theorems dbl_nats_hs.double_all_elems_loop
+ export_code double_all_elems_loop  from_list_impl  hs_it_init checking SML_imp
+
+ ML_val \<open>
+    val noi = @{code nat_of_integer}
+    val from_list_impl  = @{code from_list_impl} o map  noi
+    val double_all_elems_loop = @{code double_all_elems_loop}
+    val hs_it_init = @{code hs_it_init}
+   
+    val sinit = from_list_impl [0,1,2,3,4,5] ()
+    val doubled = double_all_elems_loop sinit hs_it_init
+  
+  \<close>
+
+
 end
